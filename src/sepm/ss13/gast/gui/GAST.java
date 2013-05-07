ae59579b97a2c417d199c8fb0155f3a7aab955ea
mathias <e1125616@student.tuwien.ac.at>
2013-05-07 11:51:46 +0200

    eclipse projekt erstellt maven & spring eingerichtet javafx hello world

---
+++ src/sepm/ss13/gast/gui/GAST.java
@@ -0,0 +1,42 @@
+package sepm.ss13.gast.gui;
+
+import org.apache.log4j.Logger;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class GAST extends Application {
+	
+	private static Logger log = Logger.getLogger(Application.class);
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		log.info("Start Application");
+		ApplicationContext ac = new ClassPathXmlApplicationContext("spring-config.xml");
+		
+		Application.launch(GAST.class, args);
+
+	}
+
+	@Override
+	public void start(Stage s) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Parent root = FXMLLoader.load(getClass().getResource("gast.fxml"));
+        
+        s.setTitle("FXML Welcome");
+        s.setScene(new Scene(root, 300, 275));
+        s.show();
+		
+	}
+
+}
