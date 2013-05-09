83dc55894bc08cf0e1b84eb0f7f253d3c3f28c2c
e 0928723 <e0928723@student.tuwien.ac.at>
2013-05-09 15:00:46 +0200

    Commit-Test

---
+++ src/sepm/ss13/gast/dao/ProduktTypDAO.java
@@ -0,0 +1,12 @@
+package sepm.ss13.gast.dao;
+
+import org.apache.log4j.Logger;
+
+/**
+ * 
+ */
+public class ProduktTypDAO {
+
+	private static final Logger log = Logger.getLogger (ProduktTypDAO.class);
+
+}
