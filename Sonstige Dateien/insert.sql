94288bd1a7a179465c63e875481b203ff35f0fa2
e 0928723 <e0928723@student.tuwien.ac.at>
2013-05-13 19:08:06 +0200

    Insert-Skript für die DB aktualisiert

--- Sonstige Dateien/insert.sql
+++ Sonstige Dateien/insert.sql
@@ -8,35 +8,35 @@ INSERT INTO produkttyp (bezeichnung, kurzbezeichnung) VALUES ('Jause', 'Jau')
 
 INSERT INTO produkttyp (bezeichnung, kurzbezeichnung) VALUES ('Wirtshausklassiker', 'Klass')
 
-INSERT INTO produkt (name, typid, preis) VALUES ('Superbier', 1, 300)
+INSERT INTO produkt (name, typid, preis) VALUES ('Superbier', 0, 300)
 
-INSERT INTO produkt (name, typid, preis) VALUES ('Megabier', 1, 400)
+INSERT INTO produkt (name, typid, preis) VALUES ('Megabier', 0, 400)
 
-INSERT INTO produkt (name, typid, preis) VALUES ('Supermegabier', 1, 500)
+INSERT INTO produkt (name, typid, preis) VALUES ('Supermegabier', 0, 500)
 
-INSERT INTO produkt (name, typid, preis) VALUES ('Wasser', 2, 100)
+INSERT INTO produkt (name, typid, preis) VALUES ('Wasser', 1, 100)
 
-INSERT INTO produkt (name, typid, preis) VALUES ('Cola', 2, 200)
+INSERT INTO produkt (name, typid, preis) VALUES ('Cola', 1, 200)
 
-INSERT INTO produkt (name, typid, preis) VALUES ('Fanta', 2, 200)
+INSERT INTO produkt (name, typid, preis) VALUES ('Fanta', 1, 200)
 
-INSERT INTO produkt (name, typid, preis) VALUES ('Frittatensuppe', 3, 300)
+INSERT INTO produkt (name, typid, preis) VALUES ('Frittatensuppe', 2, 300)
 
-INSERT INTO produkt (name, typid, preis) VALUES ('Leberknödelsuppe', 3, 350)
+INSERT INTO produkt (name, typid, preis) VALUES ('Leberknödelsuppe', 2, 350)
 
-INSERT INTO produkt (name, typid, preis) VALUES ('Nudelsuppe', 3, 300)
+INSERT INTO produkt (name, typid, preis) VALUES ('Nudelsuppe', 2, 300)
 
-INSERT INTO produkt (name, typid, preis) VALUES ('Frankfurter', 4, 400)
+INSERT INTO produkt (name, typid, preis) VALUES ('Frankfurter', 3, 400)
 
-INSERT INTO produkt (name, typid, preis) VALUES ('Berner', 4, 450)
+INSERT INTO produkt (name, typid, preis) VALUES ('Berner', 3, 450)
 
-INSERT INTO produkt (name, typid, preis) VALUES ('Wurstplatte', 4, 350)
+INSERT INTO produkt (name, typid, preis) VALUES ('Wurstplatte', 3, 350)
 
-INSERT INTO produkt (name, typid, preis) VALUES ('Wiener Schnitzel', 5, 900)
+INSERT INTO produkt (name, typid, preis) VALUES ('Wiener Schnitzel', 4, 900)
 
-INSERT INTO produkt (name, typid, preis) VALUES ('Schweinsbraten', 5, 1000)
+INSERT INTO produkt (name, typid, preis) VALUES ('Schweinsbraten', 4, 1000)
 
-INSERT INTO produkt (name, typid, preis) VALUES ('Cordon Bleu', 5, 1100)
+INSERT INTO produkt (name, typid, preis) VALUES ('Cordon Bleu', 4, 1100)
 
 INSERT INTO ware (bezeichnung, lagerstand) VALUES ('Extrawurst', 50)
 
@@ -44,14 +44,22 @@ INSERT INTO ware (bezeichnung, lagerstand) VALUES ('Käsewurst', 43)
 
 INSERT INTO ware (bezeichnung, lagerstand) VALUES ('Salami', 72)
 
-INSERT INTO bestellung (tischnummer, produktid, preis, rechnungid, status) VALUES (1, 3, 500, 1, 'offen')
+INSERT INTO rechnung (datum, trinkgeld) VALUES (NOW(), 70)
 
-INSERT INTO bestellung (tischnummer, produktid, preis, rechnungid, status) VALUES (1, 5, 200, 1, 'offen')
+INSERT INTO rechnung (datum, trinkgeld) VALUES (NOW(), 30)
 
-INSERT INTO bestellung (tischnummer, produktid, preis, rechnungid, status) VALUES (1, 13, 900, 1, 'offen')
+INSERT INTO rechnung (datum, trinkgeld) VALUES (NOW(), 20)
 
-INSERT INTO bestellung (tischnummer, produktid, preis, rechnungid, status) VALUES (12, 7, 300, 2, 'erledigt')
+INSERT INTO rechnung (datum, trinkgeld) VALUES (NOW(), 0)
 
-INSERT INTO bestellung (tischnummer, produktid, preis, rechnungid, status) VALUES (5, 6, 200, 3, 'offen')
+INSERT INTO bestellung (tischnummer, produktid, preis, rechnungid, status) VALUES (1, 2, 500, 0, 'bestellt')
 
-INSERT INTO bestellung (tischnummer, produktid, preis, rechnungid, status) VALUES (5, 10, 400, 4, 'offen')
+INSERT INTO bestellung (tischnummer, produktid, preis, rechnungid, status) VALUES (1, 4, 200, 0, 'bestellt')
+
+INSERT INTO bestellung (tischnummer, produktid, preis, rechnungid, status) VALUES (1, 12, 900, 0, 'bestellt')
+
+INSERT INTO bestellung (tischnummer, produktid, preis, rechnungid, status) VALUES (12, 6, 300, 1, 'geliefert')
+
+INSERT INTO bestellung (tischnummer, produktid, preis, rechnungid, status) VALUES (5, 5, 200, 2, 'bestellt')
+
+INSERT INTO bestellung (tischnummer, produktid, preis, rechnungid, status) VALUES (5, 9, 400, 3, 'bestellt')
