80fb904dab2fd83ed895208c82f1e18af6b49b2e
mathias <e1125616@student.tuwien.ac.at>
2013-05-13 09:17:48 +0200

    Konfiguration speichern implementiert

--- src/sepm/ss13/gast/gui/ManagementController.java
+++ src/sepm/ss13/gast/gui/ManagementController.java
@@ -4,11 +4,18 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import org.apache.log4j.Logger;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import sepm.ss13.gast.dao.DAOException;
+import sepm.ss13.gast.domain.Konfiguration;
+import sepm.ss13.gast.service.Service;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 public class ManagementController implements Initializable{
 	
@@ -17,9 +24,18 @@ public class ManagementController implements Initializable{
 		
 	 private GAST application;
 	 
+	 private ApplicationContext ac;
+	 private Service s;
+	 
+	 @FXML private Label name;
+	 @FXML private Label adresse;
+	 @FXML private Label tel;
+	 @FXML private Label logo;
+	 @FXML private Label tischanzahl;
+	 
 	 public void initialize(URL location, ResourceBundle resources) {
-			// TODO Auto-generated method stub
-			
+		 ac = new ClassPathXmlApplicationContext("spring-config.xml");
+		 //s = (Service) ac.getBean("totalesService");
 		}
 	    
 	 public void setApp(GAST application){
@@ -54,7 +70,20 @@ public class ManagementController implements Initializable{
 	     }
 	 }
 	 
-	 
+	 @FXML
+	 public void saveKonfiguration(ActionEvent e) {
+		 Konfiguration k = new Konfiguration(name.getText(),adresse.getText(),tel.getText(),null,Integer.parseInt(tischanzahl.getText()));
+		 
+		 try {
+			s.saveKonfiguration(k);
+		} catch (IllegalArgumentException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (DAOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	 }
 
 	
 
