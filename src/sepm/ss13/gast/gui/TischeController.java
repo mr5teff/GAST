37a9cd2de7cde40a68ced868034093f91b1c6f76
Patrick Seidlberger <p.seidlberger@aon.at>
2013-05-26 21:28:56 +0200

    Tischverwaltung: Implementierung fortgesetzt, Raucher/Nichtraucher- auswahl, Fehlerbehandlung

--- src/sepm/ss13/gast/gui/TischeController.java
+++ src/sepm/ss13/gast/gui/TischeController.java
@@ -9,6 +9,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Dialogs;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -31,12 +32,14 @@ public class TischeController extends Controller {
 	@FXML private TableColumn<Tisch,Integer> nummerCol;
 	@FXML private TableColumn<Tisch,Integer> plaetzeCol;
 	@FXML private TableColumn<Tisch,String> beschreibungCol;
+	@FXML private TableColumn<Tisch,String> artCol;
 	
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		s = (Service) this.getApplicationContext().getBean("GASTService");
 		nummerCol.setCellValueFactory(new PropertyValueFactory<Tisch,Integer>("nummer"));
 		plaetzeCol.setCellValueFactory(new PropertyValueFactory<Tisch,Integer>("plaetze"));
 		beschreibungCol.setCellValueFactory(new PropertyValueFactory<Tisch,String>("beschreibung"));
+		artCol.setCellValueFactory(new PropertyValueFactory<Tisch,String>("art"));
 		listTische();
 	}
 	@FXML
@@ -58,39 +61,41 @@ public class TischeController extends Controller {
 	@FXML
 	 public void addTisch() {
 		
-		/*Stage stage = GUITools.openDialog("Tisch anlegen",this.getStage());
-		GUITools.loadFXML("NeuerTischDialog.fxml", stage, this);
-		stage.show();*/
-		Tisch tisch= new Tisch();
 		
-		try {
-			s.createTisch(tisch);
-		} catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (DAOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		listTische();
+		Stage stage = GUITools.openDialog("Tisch anlegen",this.getStage());
+		Tisch t= null;
+		TischDialogController tdc = (TischDialogController)GUITools.loadFXML("TischDialog.fxml", stage, this);
+		tdc.setParentController(this);
+		tdc.setTisch(t);
+		stage.show();
+
+
 	}
 	
 	@FXML
 	 public void updateTisch(ActionEvent event) {
+		
 		Tisch t = tischeTableView.getSelectionModel().getSelectedItem();
 		 
-		if(t == null) return;
+		if(t == null) {
+			Dialogs.showInformationDialog(this.getStage(), "Keine Tisch ausgewählt!", "Tisch bearbeiten", "Information");
+			return;
+		}
 		
-		Stage stage = GUITools.openDialog("Tisch anlegen/bearbeiten",this.getStage());
-		//TischDialogController tdc = (TischDialogController) GUITools.loadFXML("TischDialog.fxml", stage, this);
-		//tdc.setTisch(t);
-		GUITools.loadFXML("TischDialog.fxml", stage, this);
+		Stage stage = GUITools.openDialog("Tisch bearbeiten",this.getStage());
+		TischDialogController tdc = (TischDialogController) GUITools.loadFXML("TischDialog.fxml", stage, this);
+		tdc.setTisch(t);
 		stage.show();
+		
 	 }
 	
 	@FXML
 	 public void deleteTisch() {
 		Tisch tisch = tischeTableView.getSelectionModel().getSelectedItem();
+		if(tisch == null) {
+			Dialogs.showInformationDialog(this.getStage(), "Keine Tisch ausgewählt!", "Tisch löschen", "Information");
+			return;
+		}
 		try {
 			s.deleteTisch(tisch);
 		} catch (IllegalArgumentException e) {
@@ -101,5 +106,11 @@ public class TischeController extends Controller {
 			e.printStackTrace();
 		}
 		listTische();
+	}
+	
+	public Tisch getSelectedTisch() {
+		Tisch t = tischeTableView.getSelectionModel().getSelectedItem();
+		 	
+		return t;
 	}
 }
